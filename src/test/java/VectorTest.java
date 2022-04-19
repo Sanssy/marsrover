b5f8b069b65f02f0de73a040350cf8540e0c94fe
Sanssy <d.sanoussy@gmail.com>
2022-04-19 12:41:00 +0200

    retrieve_backward_vector_from_a_direction

--- src/test/java/VectorTest.java
+++ src/test/java/VectorTest.java
@@ -7,13 +7,13 @@ public class VectorTest {
 
     @Test
     public void retrieve_forward_vector_from_a_direction() {
-        Assertions.assertThat(Vector.retrieveForwardTranslationFrom(Direction.EAST)).isEqualTo(new Vector(0, 1));
+        Assertions.assertThat(Vector.retrieveForwardTranslationFrom(Direction.NORTH)).isEqualTo(new Vector(0, 1));
 
-        Assertions.assertThat(Vector.retrieveForwardTranslationFrom(Direction.WEST)).isEqualTo(new Vector(0, -1));
+        Assertions.assertThat(Vector.retrieveForwardTranslationFrom(Direction.SOUTH)).isEqualTo(new Vector(0, -1));
 
-        Assertions.assertThat(Vector.retrieveForwardTranslationFrom(Direction.NORTH)).isEqualTo(new Vector(1, 0));
+        Assertions.assertThat(Vector.retrieveForwardTranslationFrom(Direction.EAST)).isEqualTo(new Vector(1, 0));
 
-        Assertions.assertThat(Vector.retrieveForwardTranslationFrom(Direction.SOUTH)).isEqualTo(new Vector(-1, 0));
+        Assertions.assertThat(Vector.retrieveForwardTranslationFrom(Direction.WEST)).isEqualTo(new Vector(-1, 0));
     }
 
     @Test
