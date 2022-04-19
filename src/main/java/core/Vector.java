d827215e625bebd9c23aefeb2aa793c082d08cb1
Sanssy <d.sanoussy@gmail.com>
2022-04-19 12:38:36 +0200

    retrieve_backward_vector_from_a_direction

--- src/main/java/core/Vector.java
+++ src/main/java/core/Vector.java
@@ -6,29 +6,41 @@ public record Vector(int x, int y) {
         return new Vector(- this.x, - this.y);
     }
 
-    public Vector retrieveForwardTranslationFrom(Direction direction) {
+    public static Vector retrieveForwardTranslationFrom(Direction direction) {
         if (direction.equals(Direction.NORTH))
-            return this.xTranslate();
+            return yTranslate();
         if (direction.equals(Direction.SOUTH))
-            return this.xNegativeTranslate();
+            return yNegativeTranslate();
         if (direction.equals(Direction.EAST))
-            return this.yTranslate();
+            return xTranslate();
         if (direction.equals(Direction.WEST))
-            return this.yNegativeTranslate();
+            return xNegativeTranslate();
         return new Vector(0,0);
     }
 
-    public Vector xTranslate() {
+    public static Vector retrieveBackwardTranslationFrom(Direction direction) {
+        if (direction.equals(Direction.SOUTH))
+            return yTranslate();
+        if (direction.equals(Direction.NORTH))
+            return yNegativeTranslate();
+        if (direction.equals(Direction.WEST))
+            return xTranslate();
+        if (direction.equals(Direction.EAST))
+            return xNegativeTranslate();
+        return new Vector(0,0);
+    }
+
+    public static Vector xTranslate() {
         return new Vector(1,0);
     }
-    public Vector yTranslate() {
+    public static Vector yTranslate() {
         return new Vector(0,1);
     }
-    public Vector xNegativeTranslate() {
-        return this.xTranslate().reverse();
+    public static Vector xNegativeTranslate() {
+        return xTranslate().reverse();
     }
-    public Vector yNegativeTranslate() {
-        return this.yTranslate().reverse();
+    public static Vector yNegativeTranslate() {
+        return yTranslate().reverse();
     }
 
 }
--- src/test/java/VectorTest.java
+++ src/test/java/VectorTest.java
@@ -7,14 +7,23 @@ public class VectorTest {
 
     @Test
     public void retrieve_forward_vector_from_a_direction() {
-        Vector vector = new Vector(1,2);
+        Assertions.assertThat(Vector.retrieveForwardTranslationFrom(Direction.EAST)).isEqualTo(new Vector(0, 1));
 
-        Assertions.assertThat(vector.retrieveForwardTranslationFrom(Direction.EAST)).isEqualTo(new Vector(0, 1));
+        Assertions.assertThat(Vector.retrieveForwardTranslationFrom(Direction.WEST)).isEqualTo(new Vector(0, -1));
 
-        Assertions.assertThat(vector.retrieveForwardTranslationFrom(Direction.WEST)).isEqualTo(new Vector(0, -1));
+        Assertions.assertThat(Vector.retrieveForwardTranslationFrom(Direction.NORTH)).isEqualTo(new Vector(1, 0));
 
-        Assertions.assertThat(vector.retrieveForwardTranslationFrom(Direction.NORTH)).isEqualTo(new Vector(1, 0));
+        Assertions.assertThat(Vector.retrieveForwardTranslationFrom(Direction.SOUTH)).isEqualTo(new Vector(-1, 0));
+    }
 
-        Assertions.assertThat(vector.retrieveForwardTranslationFrom(Direction.SOUTH)).isEqualTo(new Vector(-1, 0));
+    @Test
+    public void retrieve_backward_vector_from_a_direction() {
+        Assertions.assertThat(Vector.retrieveBackwardTranslationFrom(Direction.NORTH)).isEqualTo(new Vector(0, -1));
+
+        Assertions.assertThat(Vector.retrieveBackwardTranslationFrom(Direction.SOUTH)).isEqualTo(new Vector(0, 1));
+
+        Assertions.assertThat(Vector.retrieveBackwardTranslationFrom(Direction.EAST)).isEqualTo(new Vector(-1, 0));
+
+        Assertions.assertThat(Vector.retrieveBackwardTranslationFrom(Direction.WEST)).isEqualTo(new Vector(1, 0));
     }
 }
